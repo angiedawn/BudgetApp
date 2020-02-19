e683d0aa62b038090b44318e406bc91764de0051
Angie <newday4318@gmail.com>
2020-02-19 11:14:17 -0600

    Fields and button turn read when inputing expenses, FINISHED APP!

--- app.js
+++ app.js
@@ -168,6 +168,12 @@ var UIController = (function() {
     return (type === "exp" ? "-" : "+") + " " + int + "." + dec;
   };
 
+  nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
   return {
     getinput: function() {
       return {
@@ -240,11 +246,6 @@ var UIController = (function() {
 
     displayPercentages: function(percentages) {
       fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
-      nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
 
       nodeListForEach(fields, function(current, index) {
         if (percentages[index] > 0) {
@@ -278,6 +279,22 @@ var UIController = (function() {
         months[month] + " " + year;
     },
 
+    changedType: function() {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType +
+          "," +
+          DOMstrings.inputDescription +
+          "," +
+          DOMstrings.inputValue
+      );
+
+      nodeListForEach(fields, function(cur) {
+        cur.classList.toggle("red-focus");
+      });
+
+      document.querySelector(DOMstrings.inputBtn).classList.toggle("red");
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -298,6 +315,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     document
       .querySelector(DOM.container)
       .addEventListener("click", ctrlDeleteItem);
+
+    document
+      .querySelector(DOM.inputType)
+      .addEventListener("change", UICtrl.changedType);
   };
 
   var UpdateBudget = function() {
